f462762300a7b5a9f50e276bd39f089456b0c137
Saqib Khan <saqibkhan@utexas.edu>
2019-05-15 16:57:38 -0500

    Update
    Signed-off-by: Saqib Khan <saqibkhan@utexas.edu>

--- rtl/Adders/base_adders.v
+++ rtl/Adders/base_adders.v
@@ -7,30 +7,14 @@
 
 /////////////////////////////////////////////////////////////////////////////
 //
-// Module Name: sign bit
+// Module Name: sign bit (32-bits)
 // Function: Calculates the sign bit based on two inputs
 //
 /////////////////////////////////////////////////////////////////////////////
-module sign_bit(output wire sign,
+module sign_bit_32(output wire sign,
                 input wire[31:0] in1,
                 input wire[31:0] in2);
     xor(sign,in1[31],in2[31]);
-endmodule
-
-
-/////////////////////////////////////////////////////////////////////////////
-//
-// Module Name: Half Adder (1-bit)
-// Function: Generate the Sum and Carry out based on the just the operands
-//
-/////////////////////////////////////////////////////////////////////////////
-module half_adder(output wire sum,
-                  output wire cout,
-                  input wire in1,
-                  input wire in2);
-
-    xor(sum, in1, in2);
-    and(cout, in1, in2);
 endmodule
 
 
@@ -45,8 +29,12 @@ module full_adder(output wire sum,
                   input wire in1,
                   input wire in2,
                   input wire cin);
-    wire s1,c1,c2;
-    half_adder HA1(s1, c1, in1, in2);
-    half_adder HA2(sum, c2, s1, cin);
-    or OR1(count, c1, c2);
+    wire temp1;
+    wire temp2;
+    wire temp3;
+    xor(sum, in1, in2, cin);
+    and(temp1,in1,in2);
+    and(temp2,in1,cin);
+    and(temp3,in2,cin);
+    or(cout,temp1,temp2,temp3);
 endmodule
