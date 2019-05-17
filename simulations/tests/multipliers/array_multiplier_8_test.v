3bd26fa74e6fbc6955b54e39d38fe3ce6e7c12a3
Saqib Khan <saqibkhan@utexas.edu>
2019-05-17 10:44:01 -0500

    Add 8 and 16 bit array multiplier
    Signed-off-by: Saqib Khan <saqibkhan@utexas.edu>

---
+++ simulations/tests/multipliers/array_multiplier_8_test.v
@@ -0,0 +1,31 @@
+module test_array_multiplier_8;
+
+  // Inputs
+  reg [7:0] A;
+  reg [7:0] B;
+
+  // Outputs
+  wire [15:0] product;
+  
+  // Instantiate the Unit test (UUT)
+  array_multiplier_8 uut(
+    .product(product),
+    .A(A),
+    .B(B));
+
+  initial begin
+    A = 0;
+    B = 0;
+
+    // Wait 100 ns for global reset to finish
+    #100;
+    // Add stimulus here
+    A=8'b00000001;B=8'b00000000;
+    #10 A=8'b00001010;B=8'b00000011;
+    #10 A=8'b11010000;B=8'b10100000;
+    #10 A=8'b11111111;B=8'b11111111;
+    end
+  initial begin
+    $monitor("time=",$time,, "A=%D B=%d : product=%d",A,B,product);
+  end
+endmodule
