ff5f8a0e4a42f1af985ba9707e6c296bf189d7e1
Saqib Khan <saqibkhan@utexas.edu>
2019-06-11 16:14:04 -0500

    Clean Russian Peasant Multiplier

--- rtl/multipliers/russian_peasant/russian_peasant_modified_unsigned_multiplier_4.v
+++ rtl/multipliers/russian_peasant/russian_peasant_modified_unsigned_multiplier_4.v
@@ -1,4 +1,7 @@
 module russian_peasant_modified_unsigned_multiplier_4(product, A, B);
+    /* This implementation uses carry look-ahead adders of max length 4 even
+     * for final stage */
+
     input [3:0] A, B;
     output [7:0] product;
 
@@ -6,111 +9,151 @@ module russian_peasant_modified_unsigned_multiplier_4(product, A, B);
     wire s11, s12, s13, s14, s21, s22, s23, s24;
     wire c11, c12, c13, c14, c21, c22, c23, c24, c31, c32, c33;
 
-    wire [3:0] P0, P1, P2, P3;
-    assign P0 = A[0] ? B : 4'b0000;
-    assign P1 = A[1] ? B : 4'b0000;
-    assign P2 = A[2] ? B : 4'b0000;
-    assign P3 = A[3] ? B : 4'b0000;
+    wire [3:0] PP0, PP1, PP2, PP3;
+    assign PP0 = A[0] ? B : 4'b0000;
+    assign PP1 = A[1] ? B : 4'b0000;
+    assign PP2 = A[2] ? B : 4'b0000;
+    assign PP3 = A[3] ? B : 4'b0000;
 
-    assign product[0] = P0[0];
+    assign product[0] = PP0[0];
 
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    wire [2:0] G1;
-    wire [3:0] PP1, C1, sum1;
-    assign G1[0] = P0[1] & P1[0];
-    assign G1[1] = P0[2] & P1[1];
-    assign G1[2] = P0[3] & P1[2];
-    //assign G1[3] = 0     & P1[3];
-    assign PP1[0] = P0[1] ^ P1[0];
-    assign PP1[1] = P0[2] ^ P1[1];
-    assign PP1[2] = P0[3] ^ P1[2];
-    assign PP1[3] = P1[3];
-    //assign PP1[3] = 0     ^ P1[3];
 
-    assign C1[0] = 0;
-    assign C1[1] = G1[0];// | (PP1[0] & C1[0]);
-    assign C1[2] = G1[1] | (PP1[1] & C1[1]);
-    assign C1[3] = G1[2] | (PP1[2] & C1[2]);
-    assign cout1 = (PP1[3] & C1[3]);
-    //assign cout1 = G1[3] | (PP1[3] & C1[3]);
-    assign sum1[0] = PP1[0] ^ C1[0];
-    assign sum1[1] = PP1[1] ^ C1[1];
-    assign sum1[2] = PP1[2] ^ C1[2];
-    assign sum1[3] = PP1[3] ^ C1[3];
-   
-    assign product[1] = sum1[0];
+    wire [3:0] G1, P1, C1;
+    assign G1[0] = PP0[1] & PP1[0];
+    assign G1[1] = PP0[2] & PP1[1];
+    assign G1[2] = PP0[3] & PP1[2];
+    assign P1[0] = PP0[1] ^ PP1[0];
+    assign P1[1] = PP0[2] ^ PP1[1];
+    assign P1[2] = PP0[3] ^ PP1[2];
+    assign P1[3] = PP1[3];
 
-    /*always @(*) begin
-        $display("sum[0]=%b",sum[0]);
-        $display("sum[1]=%b",sum[1]);
-        $display("sum[2]=%b",sum[2]);
-        $display("sum[3]=%b",sum[3]);
+    assign C1[1] = G1[0];
+    assign C1[2] = G1[1] | (P1[1] & C1[1]);
+    assign C1[3] = G1[2] | (P1[2] & C1[2]);
+    assign c1    = P1[3] & C1[3]; //G[3] | (P[3] & C[3]);
+    assign s1    = P1[0];
+    assign s2    = P1[1] ^ C1[1];
+    assign s3    = P1[2] ^ C1[2];
+    assign s4    = P1[3] ^ C1[3];
 
-        $display("C[0]=%b",C[0]);
-        $display("C[1]=%b",C[0]);
-        $display("C[2]=%b",C[0]);
-        $display("C[3]=%b",C[0]);
-        $display("cout=%b",cout);
-    end*/
-    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    
-    wire [2:0] G2, PP2, C2, sum2;
-    assign G2[0] = P2[1] & P3[0];
-    assign G2[1] = P2[2] & P3[1];
-    assign G2[2] = P2[3] & P3[2];
-    assign PP2[0] = P2[1] ^ P3[0];
-    assign PP2[1] = P2[2] ^ P3[1];
-    assign PP2[2] = P2[3] ^ P3[2];
-    //assign C2[0] = 0;
-    assign C2[1] = G2[0];// | (PP2[0] & C2[0]);
-    assign C2[2] = G2[1] | (PP2[1] & C2[1]);
-    assign cout2 = G2[2] | (PP2[2] & C2[2]);
-    assign sum2[0] = PP2[0];
-    assign sum2[1] = PP2[1] ^ C2[1];
-    assign sum2[2] = PP2[2] ^ C2[2];
+    assign product[1] = s1;
+    wire [2:0] G2; /* Generate */
+    wire [2:0] P2; /* Propagate */
+    wire [2:0] C2; /* Carry */
+    assign G2[0] = PP2[1] & PP3[0];
+    assign G2[1] = PP2[2] & PP3[1];
+    assign G2[2] = PP2[3] & PP3[2];
+    assign P2[0] = PP2[1] ^ PP3[0];
+    assign P2[1] = PP2[2] ^ PP3[1];
+    assign P2[2] = PP2[3] ^ PP3[2];
+    assign C2[1] = G2[0];
+    assign C2[2] = G2[1] | (P2[1] & C2[1]);
+    assign c2    = G2[2] | (P2[2] & C2[2]);
+    assign s5    = P2[0];
+    assign s6    = P2[1] ^ C2[1];
+    assign s7    = P2[2] ^ C2[2];
 
-    /*always @(*) begin
-        $display("sum[0]=%b",sum[0]);
-        $display("sum[1]=%b",sum[1]);
-        $display("sum[2]=%b",sum[2]);
 
-        $display("C[0]=%b",C[0]);
-        $display("C[1]=%b",C[0]);
-        $display("C[2]=%b",C[0]);
-        $display("cout=%b",cout);
-    end*/
+    half_adder HA1(product[2], c3, s2, PP2[0]);
+    wire [3:0] G, P, C, sum;
+    assign G[0] = s3 & s5;
+    assign G[1] = s4 & s6;
+    assign G[2] = c1 & s7;
+    assign G[3] = PP3[3]   & c2;
+    assign P[0] = s3 ^ s5;
+    assign P[1] = s4 ^ s6;
+    assign P[2] = c1 ^ s7;
+    assign P[3] = PP3[3] ^ c2;
+
+    assign C[1] = G[0];// | (P[0] & C[0]);
+    assign C[2] = G[1] | (P[1] & C[1]);
+    assign C[3] = G[2] | (P[2] & C[2]);
+    assign product[7] = G[3] | (P[3] & C[3]);
+    assign product[3] = P[0];
+    assign product[4] = P[1] ^ C[1];
+    assign product[5] = P[2] ^ C[2];
+    assign product[6] = P[3] ^ C[3];
+endmodule
+
+
+module russian_peasant_modified_unsigned_multiplier_4_attempt4(product, A, B);
+   /* This implementation uses carry look-ahead adders of length 4, but the
+    * final stage uses 5 bit carry lookahead adder*/
+    input [3:0] A, B;
+    output [7:0] product;
+
+    wire [7:0] product;
+    wire s11, s12, s13, s14, s21, s22, s23, s24;
+    wire c11, c12, c13, c14, c21, c22, c23, c24, c31, c32, c33;
+
+    wire [3:0] PP0, PP1, PP2, PP3;
+    assign PP0 = A[0] ? B : 4'b0000;
+    assign PP1 = A[1] ? B : 4'b0000;
+    assign PP2 = A[2] ? B : 4'b0000;
+    assign PP3 = A[3] ? B : 4'b0000;
+
+    assign product[0] = PP0[0];
+
+
+    wire [3:0] G1, P1, C1;
+    assign G1[0] = PP0[1] & PP1[0];
+    assign G1[1] = PP0[2] & PP1[1];
+    assign G1[2] = PP0[3] & PP1[2];
+    assign P1[0] = PP0[1] ^ PP1[0];
+    assign P1[1] = PP0[2] ^ PP1[1];
+    assign P1[2] = PP0[3] ^ PP1[2];
+    assign P1[3] = PP1[3];
+
+    assign C1[1] = G1[0];
+    assign C1[2] = G1[1] | (P1[1] & C1[1]);
+    assign C1[3] = G1[2] | (P1[2] & C1[2]);
+    assign c1    = P1[3] & C1[3]; //G[3] | (P[3] & C[3]);
+    assign s1    = P1[0];
+    assign s2    = P1[1] ^ C1[1];
+    assign s3    = P1[2] ^ C1[2];
+    assign s4    = P1[3] ^ C1[3];
+
+
+    assign product[1] = s1;
+    wire [2:0] G2; /* Generate */
+    wire [2:0] P2; /* Propagate */
+    wire [2:0] C2; /* Carry */
+    assign G2[0] = PP2[1] & PP3[0];
+    assign G2[1] = PP2[2] & PP3[1];
+    assign G2[2] = PP2[3] & PP3[2];
+    assign P2[0] = PP2[1] ^ PP3[0];
+    assign P2[1] = PP2[2] ^ PP3[1];
+    assign P2[2] = PP2[3] ^ PP3[2];
+    assign C2[1] = G2[0];
+    assign C2[2] = G2[1] | (P2[1] & C2[1]);
+    assign c2    = G2[2] | (P2[2] & C2[2]);
+    assign s5    = P2[0];
+    assign s6    = P2[1] ^ C2[1];
+    assign s7    = P2[2] ^ C2[2]; 
 
 
     wire [4:0] G, P, C, sum;
-    assign G[0] = sum1[1]  & P2[0];
-    assign G[1] = sum1[2] & sum2[0];
-    assign G[2] = sum1[3] & sum2[1];
-    assign G[3] = cout1   & sum2[2];
-    assign G[4] = P3[3]   & cout2;
-    assign P[0] = sum1[1] ^ P2[0];
-    assign P[1] = sum1[2] ^ sum2[0];
-    assign P[2] = sum1[3] ^ sum2[1];
-    assign P[3] = cout1   ^ sum2[2];
-    assign P[4] = P3[3]   ^ cout2;
+    assign G[0] = s2 & PP2[0];
+    assign G[1] = s3 & s5;
+    assign G[2] = s4 & s6;
+    assign G[3] = c1 & s7;
+    assign G[4] = PP3[3]   & c2;
+    assign P[0] = s2 ^ PP2[0];
+    assign P[1] = s3 ^ s5;
+    assign P[2] = s4 ^ s6;
+    assign P[3] = c1 ^ s7;
+    assign P[4] = PP3[3] ^ c2;
 
-    //assign C[0] = 0;
     assign C[1] = G[0];// | (P[0] & C[0]);
     assign C[2] = G[1] | (P[1] & C[1]);
     assign C[3] = G[2] | (P[2] & C[2]);
     assign C[4] = G[3] | (P[3] & C[3]);
-    assign cout = G[4] | (P[4] & C[4]);
-    assign sum[0] = P[0];
-    assign sum[1] = P[1] ^ C[1];
-    assign sum[2] = P[2] ^ C[2];
-    assign sum[3] = P[3] ^ C[3];
-    assign sum[4] = P[4] ^ C[4];
-
-    assign product[2] = sum[0];
-    assign product[3] = sum[1];
-    assign product[4] = sum[2];
-    assign product[5] = sum[3];
-    assign product[6] = sum[4];
-    assign product[7] = cout;
+    assign product[7] = G[4] | (P[4] & C[4]);
+    assign product[2] = P[0];
+    assign product[3] = P[1] ^ C[1];
+    assign product[4] = P[2] ^ C[2];
+    assign product[5] = P[3] ^ C[3];
+    assign product[6] = P[4] ^ C[4];
 endmodule
 
 
