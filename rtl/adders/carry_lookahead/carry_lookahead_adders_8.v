d3ad50167db6c785f6c64b47eca0865f5697cd4a
Saqib Khan <saqibkhan@utexas.edu>
2019-06-19 10:11:56 -0500

    Add 16 bit russian modified multiplier
    Signed-off-by: Saqib Khan <saqibkhan@utexas.edu>

--- rtl/adders/carry_lookahead/carry_lookahead_adders_8.v
+++ rtl/adders/carry_lookahead/carry_lookahead_adders_8.v
@@ -9,22 +9,22 @@ module carry_lookahead_adder_8(output [7:0] sum,
                                 input [7:0] in1, in2,
                                 input cin);
 
-wire [7:0] G; /* Generate */
-wire [7:0] P; /* Propagate */
-wire [7:0] C; /* Carry */
+    wire [7:0] G; /* Generate */
+    wire [7:0] P; /* Propagate */
+    wire [7:0] C; /* Carry */
 
-assign G = in1 & in2; /*Generate*/
-assign P = in1 ^ in2; /*Propagate*/
+    assign G = in1 & in2; /*Generate*/
+    assign P = in1 ^ in2; /*Propagate*/
 
-assign C[0] = cin;
-assign C[1] = G[0] | (P[0] & C[0]);
-assign C[2] = G[1] | (P[1] & C[1]);
-assign C[3] = G[2] | (P[2] & C[2]);
-assign C[4] = G[3] | (P[3] & C[3]);
-assign C[5] = G[4] | (P[4] & C[4]);
-assign C[6] = G[5] | (P[5] & C[5]);
-assign C[7] = G[6] | (P[6] & C[6]);
-assign cout = G[7] | (P[7] & C[7]);
-assign sum = P ^ C;
+    assign C[0] = cin;
+    assign C[1] = G[0] | (P[0] & C[0]);
+    assign C[2] = G[1] | (P[1] & C[1]);
+    assign C[3] = G[2] | (P[2] & C[2]);
+    assign C[4] = G[3] | (P[3] & C[3]);
+    assign C[5] = G[4] | (P[4] & C[4]);
+    assign C[6] = G[5] | (P[5] & C[5]);
+    assign C[7] = G[6] | (P[6] & C[6]);
+    assign cout = G[7] | (P[7] & C[7]);
+    assign sum = P ^ C;
 endmodule
 
