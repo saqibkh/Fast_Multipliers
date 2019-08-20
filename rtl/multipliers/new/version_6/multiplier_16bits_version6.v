58137a3f7cd781d91f977b7933d87948557fa0d5
Saqib Khan <saqibkhan@utexas.edu>
2019-08-20 12:03:54 -0500

    Updates
    Signed-off-by: Saqib Khan <saqibkhan@utexas.edu>

--- rtl/multipliers/new/version_6/multiplier_16bits_version6.v
+++ rtl/multipliers/new/version_6/multiplier_16bits_version6.v
@@ -1275,6 +1275,129 @@ module kogge_stone_13(sum, cout, in1, in2);
     assign sum[12] = G_D[11] ^ P_Z[12];
 endmodule
 
+module kogge_stone_15(sum, cout, in1, in2);
+    input [14:0] in1, in2; //input
+    output [14:0] sum; //output
+    output cout; //carry-out
+    wire [14:0] G_Z, P_Z, //wires
+    G_A, P_A,
+    G_B, P_B,
+    G_C, P_C,
+    G_D, P_D,
+    G_E, P_E,
+    G_F, P_F;
+
+    assign P_Z[0]  = in1[14] ^ in2[14];
+    assign P_Z[1]  = in1[13] ^ in2[13];
+    assign P_Z[2]  = in1[12] ^ in2[12];
+    assign P_Z[3]  = in1[11] ^ in2[11];
+    assign P_Z[4]  = in1[10] ^ in2[10];
+    assign P_Z[5] = in1[9]  ^ in2[9];
+    assign P_Z[6] = in1[8]  ^ in2[8];
+    assign P_Z[7] = in1[7]  ^ in2[7];
+    assign P_Z[8] = in1[6]  ^ in2[6];
+    assign P_Z[9] = in1[5]  ^ in2[5];
+    assign P_Z[10] = in1[4]  ^ in2[4];
+    assign P_Z[11] = in1[3]  ^ in2[3];
+    assign P_Z[12] = in1[2]  ^ in2[2];
+    assign P_Z[13] = in1[1]  ^ in2[1];
+    assign P_Z[14] = in1[0]  ^ in2[0];
+
+    assign G_Z[0]  = in1[14] & in2[14];
+    assign G_Z[1]  = in1[13] & in2[13];
+    assign G_Z[2]  = in1[12] & in2[12];
+    assign G_Z[3]  = in1[11] & in2[11];
+    assign G_Z[4]  = in1[10] & in2[10];
+    assign G_Z[5] = in1[9]  & in2[9];
+    assign G_Z[6] = in1[8]  & in2[8];
+    assign G_Z[7] = in1[7]  & in2[7];
+    assign G_Z[8] = in1[6]  & in2[6];
+    assign G_Z[9] = in1[5]  & in2[5];
+    assign G_Z[10] = in1[4]  & in2[4];
+    assign G_Z[11] = in1[3]  & in2[3];
+    assign G_Z[12] = in1[2]  & in2[2];
+    assign G_Z[13] = in1[1]  & in2[1];
+    assign G_Z[14] = in1[0]  & in2[0];
+
+    /*level 1*/
+    assign G_A[0] = G_Z[0];
+    //gray_cell level_0A(1'b0, P_Z[0], G_Z[0], G_A[0]);
+    black_cell level_1A(G_Z[0],  P_Z[1],  G_Z[1],  P_Z[0],  G_A[1],  P_A[1]);
+    black_cell level_2A(G_Z[1],  P_Z[2],  G_Z[2],  P_Z[1],  G_A[2],  P_A[2]);
+    black_cell level_3A(G_Z[2],  P_Z[3],  G_Z[3],  P_Z[2],  G_A[3],  P_A[3]);
+    black_cell level_4A(G_Z[3],  P_Z[4],  G_Z[4],  P_Z[3],  G_A[4],  P_A[4]);
+    black_cell level_5A(G_Z[4],  P_Z[5],  G_Z[5],  P_Z[4],  G_A[5],  P_A[5]);
+    black_cell level_6A(G_Z[5],  P_Z[6],  G_Z[6],  P_Z[5],  G_A[6],  P_A[6]);
+    black_cell level_7A(G_Z[6],  P_Z[7],  G_Z[7],  P_Z[6],  G_A[7],  P_A[7]);
+    black_cell level_8A(G_Z[7],  P_Z[8],  G_Z[8],  P_Z[7],  G_A[8],  P_A[8]);
+    black_cell level_9A(G_Z[8],  P_Z[9],  G_Z[9],  P_Z[8],  G_A[9],  P_A[9]);
+    black_cell level_AA(G_Z[9],  P_Z[10], G_Z[10], P_Z[9],  G_A[10], P_A[10]);
+    black_cell level_BA(G_Z[10], P_Z[11], G_Z[11], P_Z[10], G_A[11], P_A[11]);
+    black_cell level_CA(G_Z[11], P_Z[12], G_Z[12], P_Z[11], G_A[12], P_A[12]);
+    black_cell level_DA(G_Z[12], P_Z[13], G_Z[13], P_Z[12], G_A[13], P_A[13]);
+    black_cell level_EA(G_Z[13], P_Z[14], G_Z[14], P_Z[13], G_A[14], P_A[14]);
+
+    /*level 2*/
+    assign G_B[1] = G_A[1];
+    //gray_cell level_1B(1'b0,      P_A[1],  G_A[1],  G_B[1]);
+    gray_cell level_2B(G_A[0],   P_A[2],  G_A[2],  G_B[2]);
+    black_cell level_3B(G_A[1],  P_A[3],  G_A[3],  P_A[1],  G_B[3],  P_B[3]);
+    black_cell level_4B(G_A[2],  P_A[4],  G_A[4],  P_A[2],  G_B[4],  P_B[4]);
+    black_cell level_5B(G_A[3],  P_A[5],  G_A[5],  P_A[3],  G_B[5],  P_B[5]);
+    black_cell level_6B(G_A[4],  P_A[6],  G_A[6],  P_A[4],  G_B[6],  P_B[6]);
+    black_cell level_7B(G_A[5],  P_A[7],  G_A[7],  P_A[5],  G_B[7],  P_B[7]);
+    black_cell level_8B(G_A[6],  P_A[8],  G_A[8],  P_A[6],  G_B[8],  P_B[8]);
+    black_cell level_9B(G_A[7],  P_A[9],  G_A[9],  P_A[7],  G_B[9],  P_B[9]);
+    black_cell level_AB(G_A[8],  P_A[10], G_A[10], P_A[8],  G_B[10], P_B[10]);
+    black_cell level_BB(G_A[9],  P_A[11], G_A[11], P_A[9],  G_B[11], P_B[11]);
+    black_cell level_CB(G_A[10], P_A[12], G_A[12], P_A[10], G_B[12], P_B[12]);
+    black_cell level_DB(G_A[11], P_A[13], G_A[13], P_A[11], G_B[13], P_B[13]);
+    black_cell level_EB(G_A[12], P_A[14], G_A[14], P_A[12], G_B[14], P_B[14]);
+
+    /*level 3*/
+    assign G_C[3] = G_B[3];
+    //gray_cell level_3C(1'b0,      P_B[3],  G_B[3],  G_C[3]);
+    gray_cell level_4C(G_A[0],   P_B[4],  G_B[4],  G_C[4]);
+    gray_cell level_5C(G_B[1],   P_B[5],  G_B[5],  G_C[5]);
+    gray_cell level_6C(G_B[2],   P_B[6],  G_B[6],  G_C[6]);
+    black_cell level_7C(G_B[3],  P_B[7],  G_B[7],  P_B[3],  G_C[7],  P_C[7]);
+    black_cell level_8C(G_B[4],  P_B[8],  G_B[8],  P_B[4],  G_C[8],  P_C[8]);
+    black_cell level_9C(G_B[5],  P_B[9],  G_B[9],  P_B[5],  G_C[9],  P_C[9]);
+    black_cell level_AC(G_B[6],  P_B[10], G_B[10], P_B[6],  G_C[10], P_C[10]);
+    black_cell level_BC(G_B[7],  P_B[11], G_B[11], P_B[7],  G_C[11], P_C[11]);
+    black_cell level_CC(G_B[8],  P_B[12], G_B[12], P_B[8],  G_C[12], P_C[12]);
+    black_cell level_DC(G_B[9],  P_B[13], G_B[13], P_B[9],  G_C[13], P_C[13]);
+    black_cell level_EC(G_B[10], P_B[14], G_B[14], P_B[10], G_C[14], P_C[14]);
+
+    /*level 4*/
+    assign G_D[7] = G_C[7];
+    //gray_cell level_7D(1'b0,       P_C[7],   G_C[7],            G_D[7]);
+    gray_cell level_8D(G_A[0],    P_C[8],   G_C[8],            G_D[8]);
+    gray_cell level_9D(G_B[1],    P_C[9],   G_C[9],            G_D[9]);
+    gray_cell level_AD(G_B[2],    P_C[10],  G_C[10],           G_D[10]);
+    gray_cell level_BD(G_C[3],    P_C[11],  G_C[11],           G_D[11]);
+    gray_cell level_CD(G_C[4],    P_C[12],  G_C[12],           G_D[12]);
+    gray_cell level_DD(G_C[5],    P_C[13],  G_C[13],           G_D[13]);
+    gray_cell level_ED(G_C[6],    P_C[14],  G_C[14],           cout);
+
+    /*outputs*/
+    assign sum[0]  =           P_Z[0];
+    assign sum[1]  = G_A[0]  ^ P_Z[1];
+    assign sum[2]  = G_B[1]  ^ P_Z[2];
+    assign sum[3]  = G_B[2]  ^ P_Z[3];
+    assign sum[4]  = G_C[3]  ^ P_Z[4];
+    assign sum[5]  = G_C[4]  ^ P_Z[5];
+    assign sum[6]  = G_C[5]  ^ P_Z[6];
+    assign sum[7]  = G_C[6]  ^ P_Z[7];
+    assign sum[8]  = G_D[7]  ^ P_Z[8];
+    assign sum[9]  = G_D[8]  ^ P_Z[9];
+    assign sum[10] = G_D[9]  ^ P_Z[10];
+    assign sum[11] = G_D[10] ^ P_Z[11];
+    assign sum[12] = G_D[11] ^ P_Z[12];
+    assign sum[13] = G_D[12] ^ P_Z[13];
+    assign sum[14] = G_D[13] ^ P_Z[14];
+endmodule
+
 module kogge_stone_16(sum, cout, in1, in2);
     input [15:0] in1, in2; //input
     output [15:0] sum; //output
