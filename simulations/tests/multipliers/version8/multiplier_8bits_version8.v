9213a7643f2f1eae98f4dd6b32afe537fb8cb8c2
Saqib Khan <saqibkhan@utexas.edu>
2019-08-21 09:25:03 -0500

    Add test and scripts for new implementations
    Signed-off-by: Saqib Khan <saqibkhan@utexas.edu>

---
+++ simulations/tests/multipliers/version8/multiplier_8bits_version8.v
@@ -0,0 +1,32 @@
+module test_multiplier_8bits_version8;
+
+  // Inputs
+  reg [7:0] A;
+  reg [7:0] B;
+
+  // Outputs
+  wire [15:0] product;
+  
+  // Instantiate the Unit test (UUT)
+  multiplier_8bits_version8 uut(
+    .product(product),
+    .A(A),
+    .B(B));
+
+  initial begin
+    A = 0;
+    B = 0;
+
+    // Wait 100 ns for global reset to finish
+    #100;
+    // Add stimulus here
+    //A=4'b0001;B=4'b0000;
+    #10 A=8'b01100010;B=8'b01110011;
+    #10 A=8'b10101010;B=8'b01100011;
+    #10 A=8'b11100101;B=8'b00101010;
+    end
+  initial begin
+    //$monitor("time=",$time,, "A=%b B=%b : product=%b",A,B,product);
+    $monitor("time=",$time,, "A=%d B=%d : product=%d",A,B,product);
+  end
+endmodule
