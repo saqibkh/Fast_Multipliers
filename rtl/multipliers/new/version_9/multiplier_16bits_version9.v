9d4ea963fba265a1db140b82bdc54b4167bfce9f
Saqib Khan <saqibkhan@utexas.edu>
2019-08-28 22:05:54 -0500

    Update multiplier_16bits_version9.v

--- rtl/multipliers/new/version_9/multiplier_16bits_version9.v
+++ rtl/multipliers/new/version_9/multiplier_16bits_version9.v
@@ -4010,3 +4010,18 @@ module half_adder(output wire sum,
     xor(sum, in1, in2);
     and(cout, in1, in2);
 endmodule
+
+module full_adder(output wire sum,
+                  output wire cout,
+                  input wire in1,
+                  input wire in2,
+                  input wire cin);
+    wire temp1;
+    wire temp2;
+    wire temp3;
+    xor(sum, in1, in2, cin);
+    and(temp1,in1,in2);
+    and(temp2,in1,cin);
+    and(temp3,in2,cin);
+    or(cout,temp1,temp2,temp3);
+endmodule
