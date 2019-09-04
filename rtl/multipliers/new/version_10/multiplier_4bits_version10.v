77d75c6da8252e41443910091b9b15ff1dffa8f7
Saqib Khan <saqibkhan@utexas.edu>
2019-09-03 23:43:40 -0500

    Updates
    Signed-off-by: Saqib Khan <saqibkhan@utexas.edu>

--- rtl/multipliers/new/version_10/multiplier_4bits_version10.v
+++ rtl/multipliers/new/version_10/multiplier_4bits_version10.v
@@ -2,6 +2,58 @@ module multiplier_4bits_version10(product, A, B);
 
     /* This approach tries to use the longest kogge-stone adder as much as
      * possible using the dadda approach of minimum reduction in each step.
+     * Area:
+     * Power: mW
+     * Timing: ns
+     */
+
+    output [7:0] product;
+    input [3:0] A, B;
+
+    wire [3:0] pp0;
+    wire [3:0] pp1;
+    wire [3:0] pp2;
+    wire [3:0] pp3;
+
+
+    assign pp0 = A[0] ? B: 4'b0000;
+    assign pp1 = A[1] ? B: 4'b0000;
+    assign pp2 = A[2] ? B: 4'b0000;
+    assign pp3 = A[3] ? B: 4'b0000;
+
+
+    /*Stage 1*/
+    wire[3:0] s1, in1_1, in1_2;
+    wire c1;
+    assign in1_1 = {pp0[2],pp0[3],pp1[3], pp2[3]};
+    assign in1_2 = {pp1[1],pp1[2],pp2[2], pp3[2]};
+    kogge_stone_4 KS_1(s1, c1, in1_1, in1_2);
+
+    wire[1:0] s1, in2_1, in2_2;
+    assign in1_1 = {pp2[1], pp3[1]};
+    assign in1_2 = {pp3[0], 1'b0};
+    kogge_stone_2 KS_2(s2, c2, in2_1, in2_2);
+
+    /*Final Stage */
+    wire[5:0] s, in_1, in_2;
+    wire c;
+    assign in_1 = {pp0[1],pp2[0],s1[1],s1[2], s1[3],pp3[3]};
+    assign in_2 = {pp1[0],s1[0], s2[0],s2[1], c2,   c1};
+    kogge_stone_6 KS(s, c, in_1, in_2);
+
+    assign product[0] = pp0[0];
+    assign product[1] = s[0];
+    assign product[2] = s[1];
+    assign product[3] = s[2];
+    assign product[4] = s[3];
+    assign product[5] = s[4];
+    assign product[6] = s[5];
+    assign product[7] = c;
+
+module multiplier_4bits_version10_attempt1(product, A, B);
+
+    /* This approach tries to use the longest kogge-stone adder as much as
+     * possible using the dadda approach of minimum reduction in each step.
      * Area: 283.926491
      * Power: 0.1013mW
      * Timing: 0.69ns
