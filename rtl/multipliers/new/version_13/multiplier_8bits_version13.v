7431121b2c3edb60450a77e7f3c0270f39a78d70
Saqib Khan <saqibkhan@utexas.edu>
2019-09-11 21:24:14 -0500

    Update multiplier_8bits_version13.v

--- rtl/multipliers/new/version_13/multiplier_8bits_version13.v
+++ rtl/multipliers/new/version_13/multiplier_8bits_version13.v
@@ -28,75 +28,50 @@ module multiplier_8bits_version13(product, A, B);
     assign pp6 = A[6] ? B: 8'b00000000;
     assign pp7 = A[7] ? B: 8'b00000000;
 
-
-    /*Stage 1*/
-    wire[3:0] s1, in1_1, in1_2;
+/* Stage 1*/
+    wire[7:0] s1, in1_1, in1_2;
     wire c1;
-    assign in1_1 = {pp0[5],pp0[6],pp0[7],pp1[7]};
-    assign in1_2 = {pp1[4],pp1[5],pp1[6],pp2[6]};
-    CLA_4 KS_1(s1, c1, in1_1, in1_2);
-    wire[3:0] s2, in2_1, in2_2;
+    assign in1_1 = {pp0[4], pp0[5], pp0[6], pp0[7], pp1[7], pp2[7], pp3[7], pp4[7]};
+    assign in1_2 = {pp1[3], pp1[4], pp1[5], pp1[6], pp2[6], pp3[6], pp4[6], pp5[6]};
+    CLA_8 KS_1(s1, c1, in1_1, in1_2);
+
+    wire[5:0] s2, in2_1, in2_2;
     wire c2;
-    assign in2_1 = {pp2[4],pp2[5],pp3[5],pp2[7]};
-    assign in2_2 = {pp3[3],pp3[4],pp4[4],pp3[6]};
-    CLA_4 KS_2(s2, c2, in2_1, in2_2);
+    assign in2_1 = {pp2[3], pp2[4], pp2[5], pp3[5], pp4[5], pp5[5]};
+    assign in2_2 = {pp3[2], pp3[3], pp3[4], pp4[4], pp5[4], pp6[4]};
+    CLA_6 KS_2(s2, c2, in2_1, in2_2);
+
     wire[3:0] s3, in3_1, in3_2;
     wire c3;
-    assign in3_1 = {pp4[3],pp5[3],pp4[5],pp3[7]};
-    assign in3_2 = {pp5[2],pp6[2],pp5[4],pp4[6]};
+    assign in3_1 = {pp4[2], pp4[3], pp5[3], pp6[3]};
+    assign in3_2 = {pp5[1], pp5[2], pp6[2], pp7[2]};
     CLA_4 KS_3(s3, c3, in3_1, in3_2);
 
-    /*Stage 2*/
-    wire[3:0] s4, in4_1, in4_2;
+    wire[1:0] s4, in4_1, in4_2;
     wire c4;
-    assign in4_1 = {pp0[3],pp0[4],pp2[3],pp4[2]};
-    assign in4_2 = {pp1[2],pp1[3],pp3[2],pp5[1]};
-    CLA_4 KS_4(s4, c4, in4_1, in4_2);
-    wire[3:0] s5, in5_1, in5_2;
+    assign in4_1 = {pp6[1], 1'b0};
+    assign in4_2 = {pp7[0], pp7[1]};
+    CLA_2 KS_4(s4, c4, in4_1, in4_2);
+
+    /* Stage 2*/
+    wire[11:0] s5, in5_1, in5_2;
     wire c5;
-    assign in5_1 = {pp2[2],pp4[1],pp6[0],pp6[1]};
-    assign in5_2 = {pp3[1],pp5[0],s1[1],pp7[0]};
-    CLA_4 KS_5(s5, c5, in5_1, in5_2);
-    wire[3:0] s6, in6_1, in6_2;
+    assign in5_1 = {pp0[2], pp0[3], pp2[2], pp4[1], pp6[0], s1[3], s1[4], s1[5], pp7[3], pp7[4], pp7[5], pp6[7]};
+    assign in5_2 = {pp1[1], pp1[2], pp3[1], pp5[0], s1[2],  s2[2], s2[3], s2[4], s1[6],  pp6[5], pp6[6], pp7[6]};
+    CLA_12 KS_5(s5, c5, in5_1, in5_2);
+
+    wire[9:0] s6, in6_1, in6_2;
     wire c6;
-    assign in6_1 = {s2[1],pp7[1],pp6[3],pp5[5]};
-    assign in6_2 = {s3[0],1'b0,pp7[2],pp6[4]};
-    CLA_4_c KS_6(s6, c6, in6_1, in6_2, s1[2]);
-    wire[3:0] s7, in7_1, in7_2;
-    wire c7;
-    assign in7_1 = {s2[2],c1,pp7[3],pp4[7]};
-    assign in7_2 = {s3[1],s2[3],c2,pp5[6]};
-    CLA_4_c KS_7(s7, c7, in7_1, in7_2, s1[3]);
-    wire[1:0] s8, in8_1, in8_2;
-    wire c8;
-    assign in8_1 = {pp7[4],pp5[7]};
-    assign in8_2 = {c3,pp6[6]};
-    CLA_2_c KS_8(s8, c8, in8_1, in8_2, pp6[5]);
-
-    /*Stage 3*/
-    wire[3:0] s9, in9_1, in9_2;
-    wire c9;
-    assign in9_1 = {pp0[2],pp2[1],pp4[0],s1[0]};
-    assign in9_2 = {pp1[1],pp3[0],s4[1],s4[2]};
-    CLA_4 KS_9(s9, c9, in9_1, in9_2);
-    wire[3:0] s10, in10_1, in10_2;
-    wire c10;
-    assign in10_1 = {s4[3],c4,c5,s3[2]};
-    assign in10_2 = {s5[2],s5[3],s6[1],s6[2]};
-    CLA_4_c KS_10(s10, c10, in10_1, in10_2, s2[0]);
-    wire[3:0] s11, in11_1, in11_2;
-    wire c11;
-    assign in11_1 = {s6[3],c6,pp7[5],pp6[7]};
-    assign in11_2 = {s7[2],s7[3],c7,pp7[6]};
-    CLA_4_c KS_11(s11, c11, in11_1, in11_2, s3[3]);
-
-
-    /*Final Stage 3*/
+    assign in6_1 = {pp2[1], pp4[0], s1[1], s2[1], s3[1], s3[2], s3[3], s2[5], s1[7], pp5[7]};
+    assign in6_2 = {pp3[0], s1[0],  s2[0], s3[0], s4[0], s4[1], c4,    c3,    c2,    c1};
+    CLA_10 KS_6(s6, c6, in6_1, in6_2);
+    
+    /* Final Stage */
     wire[13:0] s, in_1, in_2;
     wire c;
-    assign in_1 = {pp0[1],pp2[0],s4[0],s5[0],s5[1],c9,s6[0],s7[0],s7[1],c10,s8[0],s8[1],c8,pp7[7]};
-    assign in_2 = {pp1[0],s9[0],s9[1],s9[2],s9[3],s10[0],s10[1],s10[2],s10[3],s11[0],s11[1],s11[2],s11[3],c11};
-    kogge_stone_14(s, c, in_1, in_2);
+    assign in_1 = {pp0[1], pp2[0], s5[1], s5[2], s5[3], s5[4], s5[5], s5[6], s5[7], s5[8], s5[9], s5[10], s5[11], pp7[7]};
+    assign in_2 = {pp1[0], s5[0], s6[0],  s6[1], s6[2], s6[3], s6[4], s6[5], s6[6], s6[7], s6[8], s6[9],  c6,     c5};
+    kogge_stone_14 KS(s, c, in_1, in_2);
 
     assign product[0] = pp0[0];
     assign product[1] = s[0];
