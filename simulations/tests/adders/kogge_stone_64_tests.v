91c7bf3d8214c15dba46ad2948499ee950f0c14b
Saqib Khan <saqibkhan@utexas.edu>
2019-09-30 22:09:10 -0500

    Add KSA 64 bits
    Signed-off-by: Saqib Khan <saqibkhan@utexas.edu>

---
+++ simulations/tests/adders/kogge_stone_64_tests.v
@@ -0,0 +1,35 @@
+module test_kogge_stone_64;
+
+  // Inputs
+  reg [63:0] in1;
+  reg [63:0] in2;
+  reg cin;
+
+  // Outputs
+  wire [63:0] sum;
+  wire cout;
+  
+  // Instantiate the Unit test (UUT)
+  kogge_stone_64 uut(
+    .sum(sum),
+    .cout(cout),
+    .in1(in1),
+    .in2(in2),
+    .cin(cin));
+
+  initial begin
+    in1 = 0;
+    in2 = 0;
+    cin = 0;
+
+    // Wait 100 ns for global reset to finish
+    #100;
+    // Add stimulus here
+    in1=64'h0000000000000001;in2=64'h0000000000000000;cin=1'b0;
+    #10 in1=64'h0000000000001010;in2=64'h0000000000000011;cin=1'b0;
+    #10 in1=64'h1101000000000000;in2=64'h1010000000000000;cin=1'b1;
+    end
+  initial begin
+    $monitor("time=",$time,, "in1=%d in2=%d cin=%d : sum=%d cout=%d",in1,in2,cin,sum,cout);
+  end
+endmodule
